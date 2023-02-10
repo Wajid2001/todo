cd21d6f80e69a5b5daa0f91e597696c0a4da7e03
Wajid Areeb <67428865+Wajid2001@users.noreply.github.com>
2023-02-10 07:54:55 +0530

    feat: initial project with tailwind, vite, and fluent ui 9

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@fluentui/react-components';
+
+function App() {
+  return (
+    <div>
+      <Button appearance='primary'>Get started</Button>
+      <h1 className='text-3xl font-bold underline'>Todo</h1>
+    </div>
+  );
+}
+
+export default App;
